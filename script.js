555c2df9afe9d5dfeab514f52483490f2eaa9957
igbayilolaoa <olamilekan.igbayilola@hotmail.com>
2023-07-04 13:13:32 +0100

    Fieldco website clone

---
+++ script.js
@@ -0,0 +1,20 @@
+var ayo = "joy";
+
+var oyin = "bad charger";
+let ife = "dell latitude";
+console.log (ife);
+
+var jubril = "ziad";
+var aramide = "Tutor Jubril"
+console.log (aramide);
+// prompt("what is your name?")
+
+var first = 44
+var second = 88
+console.log (first + second)
+
+var third = "lekan"
+var fourth = 90
+console.log (third + fourth)
+
+console.log(first * fourth)
